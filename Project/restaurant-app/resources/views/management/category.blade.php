2af8887bf52bbb2f236dd09a215d0874179af151
Oussama Almesskine <oussama.almesskine@gmail.com>
2021-08-03 11:48:19 +0100

    READ & DISPLAY ALL CATEGORIES

--- Project/restaurant-app/resources/views/management/category.blade.php
+++ Project/restaurant-app/resources/views/management/category.blade.php
@@ -15,6 +15,7 @@
                     <i class="fa fa-align-justify"></i> Category
                     <a href="/management/category/create" class="btn btn-success btn-sm float-right"><i class="fas fa-plus"></i> Create Category</a>
                     <hr>
+
                     @if(Session()->has('status'))
 
                         <div class="alert alert-success">
@@ -23,8 +24,38 @@
                         </div>
 
                     @endif
+
+                <table class=" table table-bordered">
+
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                         @foreach($categories as $category)
+                                <tr>
+                                    <th scoop="row">{{$category->id}}</th>
+                                    <td>{{$category->name}}</td>
+                                    <td>
+                                        <a href="management/category/{{$category->id}/edit" class="btn btn-warning">Edit</a>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+
+                         @endforeach
+                    </tbody>
+
+                </table>
+                {{$categories->links()}}
+
             </div>
     </div>
 </div>
-
 @endsection
