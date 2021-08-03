680d1e618342e863bd09b504cd373c28089e3651
Oussama Almesskine <oussama.almesskine@gmail.com>
2021-08-03 16:52:23 +0100

    Create a Menu Model

--- Project/restaurant-app/resources/views/management/index.blade.php
+++ Project/restaurant-app/resources/views/management/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-            @include('management.inc.menu')
+            @include('management.inc.sidebar')
         <div class="col-md-8">
         </div>
     </div>
